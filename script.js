00b5578661e868243951ebdc612c03e336db522e
enoupluscareer <65179024+enoupluscareer@users.noreply.github.com>
2020-05-26 17:49:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function openMenu()
+{
+    document.getElementById("sidebar").style.display="block";
+    document.getElementById("overlay").style.display="block";
+}
+function closeMenu()
+{
+    document.getElementById("sidebar").style.display="none";
+    document.getElementById("overlay").style.display="none";
+}
